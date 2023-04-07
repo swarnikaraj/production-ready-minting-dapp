45724dd4f12726d75e2b426ab6964f78f3737143
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2023-04-08 03:04:47 +0530

    mint button

--- frontend-react-app/src/App.js
+++ frontend-react-app/src/App.js
@@ -1,12 +1,34 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [amount, setAmount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <div>Minting unavailable</div>
-      </header>
+      <div className="container">
+        <div>
+          <div className="flex">
+            <button
+              disabled={amount <= 0}
+              onClick={() => setAmount(amount - 1)}
+              className="bg-white text-black px-3"
+            >
+              -
+            </button>
+            <div className="px-4">{amount}</div>
+            <button
+              onClick={() => setAmount(amount + 1)}
+              className="bg-white text-black px-3"
+            >
+              +
+            </button>
+          </div>
+        </div>
+
+        <div className="bg-[#aaa6a6] text-black px-4 py-2 mt-4">
+          Minting unavailable
+        </div>
+      </div>
     </div>
   );
 }
